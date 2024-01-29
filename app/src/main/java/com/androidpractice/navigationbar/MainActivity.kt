ca25c92ddc786cc6e69771a69d24d93026fe5c0d
AyiokaMecha <ayiokamechadaniel@gmail.com>
2024-01-29 04:32:50 +0300

    All the necessary imports for the application to run smoothly

--- app/src/main/java/com/androidpractice/navigationbar/MainActivity.kt
+++ app/src/main/java/com/androidpractice/navigationbar/MainActivity.kt
@@ -3,13 +3,38 @@ package com.androidpractice.navigationbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.androidpractice.navigationbar.ui.theme.NavigationBarTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,6 +54,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
@@ -42,6 +68,12 @@ fun MainScreen() {
     )
 
 
+}
+
+sealed class NavRoutes(val route: String) {
+    object Home : NavRoutes("home")
+    object Contacts : NavRoutes("contacts")
+    object Favorites : NavRoutes("favorites")
 }
 
 @Composable
@@ -73,11 +105,7 @@ fun GreetingPreview() {
 
 //to be placed in other files
 
-sealed class NavRoutes(route: String) {
-    object Home : NavRoutes("home")
-    object Contacts : NavRoutes("contacts")
-    object Favorites : NavRoutes("favorites")
-}
+
 
 data class BarItem(
     val title: String,
@@ -164,8 +192,7 @@ fun BottomNavigationBar(navController: NavHostController) {
                 onClick = {
                     navController.navigate(navItem.route) {
                         popUpTo(
-                            navController.graph.findStartDesti
-                                    nation ().id
+                            navController.graph.findStartDestination().id
                         ) {
                             saveState = true
                         }
